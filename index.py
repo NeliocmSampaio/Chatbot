452fba5848be4c0d50e14dbfee7e20344dab70b5
Nélio <neliocmsampaio@gmail.com>
2017-08-25 15:58:00 -0300

    third

--- index.py
+++ index.py
@@ -10,3 +10,12 @@ def webhook():
 
 if __name__ == '__main__':
 	app.run(debug=true)
+
+def webhook():
+    if request.method == 'POST':
+        pass
+    elif request.method == 'GET': # Para a verificação inicial
+        if request.args.get('hub.verify_token') == os.environ.get('FB_VERIFY_TOKEN'):
+            return request.args.get('hub.challenge')
+        return "Wrong Verify Token"
+    return "Nothing"
